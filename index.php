aefdde0bf40ab171b9bda941f209787001f723cb
RaulAM <raulartilesm@gmail.com>
2024-10-25 13:36:59 +0100

    Update

--- index.php
+++ index.php
@@ -1,18 +1,16 @@
 <?php
-
 // Definir classes con constructor de la manera antigua
-
-
 class Car {
     public $make;
     public $model;
     public $year;
-
     public function __construct(string $make, string $model, int $year) {
         $this->make = $make;
         $this->model = $model;
         $this->year = $year;
     }
+    public function __destruct() {
+        echo 'Distroying: '. $this->make.' '.$this->model.PHP_EOL;
+    }
 }
-
 $myCar = new Car('Toyota', 'Corolla', 2020);
