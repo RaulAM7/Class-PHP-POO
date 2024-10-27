e88d1ab302f5555fc53bfa4e9520443fda1bc42a
RaulAM <raulartilesm@gmail.com>
2024-10-27 19:16:05 +0000

    Update agregando multiples metodos de instanciacion a una clase pillando diferentes inputs

--- index.php
+++ index.php
@@ -24,9 +24,34 @@ class Car
     {
         echo 'Distroying: '. $this->make.' '.$this->model.PHP_EOL;
     }
+    public static function instanceFromBasicData(string $make, string $model, int $year): static
+    {
+        $newCar = new static($make, $model, $year);
+        return $newCar;
+    }
+    public static function instanceFromArray(array $arr): static 
+    {
+        $data = $arr;
+        return new static ($data['make'], $data['model'], $data['year']);
+
+    }
+    public static function instanceFromJson(string $json): static
+    {
+        $data = json_decode($json, true);
+        return new static ($data['make'], $data['model'], $data['year']);
+    }
+    public static function instanceFromXml(string $xml): static
+    {
+        //define convert_xml_to_array($xml), la siguiente linea es $data = convert_xml_to_array($xml)
+        $data = $xml;
+        return new static ($data['make'], $data['model'], $data['year']);
+    }
 }
-$myCar = new Car('Toyota', 'Corolla', 2020);
-print_r($myCar->getProperties());
+$myCar = Car::instanceFromBasicData('Toyota', 'Corolla', 2020);
+print_r($myCar->getProperties()).PHP_EOL;
+print_r($myCar->make).PHP_EOL;
+print_r($myCar->model).PHP_EOL;
+print_r($myCar->year);
 
 
 // Definir propiedades con el nuevo metodo de PHP 8: promoted properties in constructor
