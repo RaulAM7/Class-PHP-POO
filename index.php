fc57d71110bbae48526211a28f2efd21a926c95b
RaulAM <raulartilesm@gmail.com>
2024-10-27 18:21:00 +0000

    Update: new promoted properties construct method PHP 8

--- index.php
+++ index.php
@@ -1,16 +1,40 @@
 <?php
 // Definir classes con constructor de la manera antigua
-class Car {
+class Car 
+{
     public $make;
     public $model;
     public $year;
-    public function __construct(string $make, string $model, int $year) {
+    
+    public function __construct(
+        string $make, 
+        string $model, 
+        int $year
+    ) 
+    {
         $this->make = $make;
         $this->model = $model;
         $this->year = $year;
     }
-    public function __destruct() {
+    public function __destruct()
+    {
         echo 'Distroying: '. $this->make.' '.$this->model.PHP_EOL;
     }
 }
 $myCar = new Car('Toyota', 'Corolla', 2020);
+
+// Definir propiedades con el nuevo metodo de PHP 8: promoted properties in constructor
+class Motorbike 
+{
+    public function __construct(
+        public string $make, 
+        public string $model, 
+        public int $year
+    ) 
+    {
+        $this->make = $make;
+        $this->model = $model;
+        $this->year = $year;
+    }
+}
+
