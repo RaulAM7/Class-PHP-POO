53cd8bdc37cd5bafb698ed4f30d4a067f0f95c35
RaulAM <raulartilesm@gmail.com>
2024-10-27 19:55:27 +0000

    Update mejorando metodos de instanciacion

--- index.php
+++ index.php
@@ -24,30 +24,46 @@ class Car
     {
         echo 'Distroying: '. $this->make.' '.$this->model.PHP_EOL;
     }
-    public static function instanceFromBasicData(string $make, string $model, int $year): static
+    protected static function instanceFromBasicData(string $make, string $model, int $year): static
     {
         $newCar = new static($make, $model, $year);
         return $newCar;
     }
-    public static function instanceFromArray(array $arr): static 
+    protected static function instanceFromArray(array $arr): static 
     {
         $data = $arr;
         return new static ($data['make'], $data['model'], $data['year']);
 
     }
-    public static function instanceFromJson(string $json): static
+    protected static function instanceFromJson(string $json): static
     {
         $data = json_decode($json, true);
         return new static ($data['make'], $data['model'], $data['year']);
     }
-    public static function instanceFromXml(string $xml): static
+    protected static function instanceFromXml(string $xml): static
     {
         //define convert_xml_to_array($xml), la siguiente linea es $data = convert_xml_to_array($xml)
         $data = $xml;
         return new static ($data['make'], $data['model'], $data['year']);
     }
+    public static function create($source, ...$args): static 
+    {
+        if (count($args) === 2) {
+            return self::instanceFromBasicData($source, $args[0], $args[1]);
+        }
+        if (is_array($source)) {
+            return self::instanceFromArray($source);
+        }
+        if (is_string($source)) {
+            $jsonData = json_decode($source, true);
+            if ($jsonData !== null) {
+                return self::instanceFromJson($source);
+            }
+        }
+        throw new \InvalidArgumentException('Invalid parameters');
+    }
 }
-$myCar = Car::instanceFromBasicData('Toyota', 'Corolla', 2020);
+$myCar = Car::create('Toyota', 'Corolla', 2020);
 print_r($myCar->getProperties()).PHP_EOL;
 print_r($myCar->make).PHP_EOL;
 print_r($myCar->model).PHP_EOL;
